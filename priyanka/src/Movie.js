3e6e8790ff23dc712fedaefe29362561db7ae889
Priyanka050702 <Priyankaammu05@gmail.com>
2024-02-28 11:31:13 +0530

    first commit

---
+++ priyanka/src/Movie.js
@@ -0,0 +1,13 @@
+function Movie()
+{
+    let value=20;
+    return(
+        <div>
+            <img src ="https://occ-0-5261-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABdv0IRVoa_UJz_c6FtBX9LFjmXOT7cRwvGsYpmgDZda-VoFuEZtSq6tLFm0JJ61PApztV2q8p8zpFXyPRM8BYLhP5OJm9X3fWGR20WEXVyU4V_8Ec5FmTp2eirM-0LtFTQx3g6dmFWJdF9ERn95UScGi28QSBE6Xogs.jpg?r=434" />
+                <h2> Animal</h2>
+                <p>{value}</p>
+                
+        </div>
+    )
+}
+export default Movie;
---
+++ priyanka/src/Movies.js
@@ -0,0 +1,17 @@
+import Movie from "./Movie";
+function Movies()
+{
+    return (
+        <div className="movies">
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+
+
+
+        </div>
+    )
+}
+export default Movies;
